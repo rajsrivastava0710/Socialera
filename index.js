3cc93c2e872cc7944f3e873ea857c444c6387d1e
Raj <rajsriv.14@gmail.com>
2020-01-07 15:44:13 +0530

    Directory Structure completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const db = require("./config/mongoose");
 const port = 8000;
 
 app.use(express.static('./assets'));
