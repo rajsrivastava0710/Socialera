ec9dffa85d26b3a263a526a4c1ee6346df57c4c5
Raj <rajsriv.14@gmail.com>
2020-01-08 12:38:09 +0530

    Back to no auth

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,10 +1,32 @@
+const User = require("../models/user");
+
 module.exports.profile = function(req,res){
-	return res.render('profile',{
-		title:'ProfilePage'
+			return res.render('profile',{
+			title:'ProfilePage'
+		});
+	}
+module.exports.login = function(req,res){
+	return res.render('login',{
+		title:'LoginPage'
 	});
 }
-module.exports.new = function(req,res){
-	return res.render('newUser',{
-		title:'NewUserPage'
+module.exports.signup = function(req,res){
+	return res.render('signup',{
+		title:'SignUpPage'
 	});
+}
+
+// get signup data
+module.exports.create = function(req,res){
+	
+}
+
+// signin and create session for user
+module.exports.createSession = function(req,res){
+	
+
+}
+
+module.exports.removeSession = function(req,res){
+	
 }
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,16 @@ const router = express.Router();
 
 console.log("User Router loaded");
 
-router.get('/',userController.profile);
+router.get('/profile',userController.profile);
 
-router.get('/new',userController.new);
+router.get('/login',userController.login);
+
+router.get('/signup',userController.signup);
+
+router.post('/create',userController.create);
+
+router.post('/create-session',userController.createSession);
+
+router.post('/remove-session',userController.removeSession)
 
 module.exports = router;
