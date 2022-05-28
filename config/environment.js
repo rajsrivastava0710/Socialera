73f7097c368b92d060fd6f43dff50850756e9dc8
Raj <rajsriv.14@gmail.com>
2022-05-28 15:46:01 +0530

    Random minor change

--- config/environment.js
+++ config/environment.js
@@ -14,35 +14,6 @@ const accessLogStream = rfs.createStream('access.log', {
   compress:'gzip'
 })
 
-const development = {
-	name:'development',
-	port:8000,
-	asset_path:'/assets',
-	upload_path:'/uploads',
-	session_cookie_key:"weFR_3283n}}43$#8fnf12_ef",
-	db:'CNFB_db',
-	gmail_id:"rajsriv.14@gmail.com",
-	gmail_password:"rj071014@gmail",
-	google_client_id:"1062128411018-28svnet4lk4c3un619klibvv0t2pg3nt.apps.googleusercontent.com",
-	google_client_secret:"IesopoFyMrldK2JpEf8LnZkw",
-	google_callback_url:"http://localhost:8000/users/auth/google/callback",
-	github_client_id:"b050085d98a316c7e03c",	
-	github_client_secret:"f4b234a1adabf3335ef37f9a4565b808e5615703",
-	github_callback_url:"http://localhost:8000/users/auth/github/callback",
-	fb_client_id:"**********",
-	fb_client_secret:"**********",
-	fb_callback_url:"**********",
-	jwt_secret:"hjHj93jKh[Igkjei8khk_R_fr",
-	morgan:{
-		mode:'dev',
-		options:{ stream: accessLogStream,
-					skip: function (req, res) { 
-						return res.statusCode < 400
-				 	}
-		}
-	}
-}
-
 const production = {
 	name:'production',
 	port:8000,
@@ -73,4 +44,5 @@ const production = {
 }
 
 // module.exports = eval(process.env.NODE_ENV == undefined ? development:eval(process.env.NODE_ENV));
-module.exports = development;
+// module.exports = development;
+module.exports = production
